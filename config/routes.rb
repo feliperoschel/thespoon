a6db1085adca76d673fb75401494e448dea2861e
Felipe Roschel <f.roschel@gmail.com>
2019-07-22 23:29:11 +0800

    thespoon on rails

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,13 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+
+  # get 'pages/contact'
+  get 'contact', to: 'pages#contact', as: :contact
+
+  # get 'pages/about'
+  get 'about', to: 'pages#about', as: :about
+
+  get 'restaurants', to: 'pages#index', as: :restaurants
+
+  root to: 'pages#home'
 end
